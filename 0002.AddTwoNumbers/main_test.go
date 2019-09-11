3f9d3849a65155674d0104f032cc876dd1f53777
nullptru <nullptru@gmail.com>
2019-09-11 09:33:45 +0800

    Fix: Problem 2 test

--- 0002.AddTwoNumbers/main_test.go
+++ 0002.AddTwoNumbers/main_test.go
@@ -1,4 +1,4 @@
-package leetcode 
+package leetcode
 
 import (
 	"fmt"
@@ -6,45 +6,57 @@ import (
 )
 
 func L2S(list *ListNode) []int {
-	return []int {
-		list.Val,
-		list.Next.Val,
-		list.Next.Next.Val,
+	r := []int{}
+	for list != nil {
+		r = append(r, list.Val)
+		list = list.Next
 	}
+	return r
 }
 
 func S2L(nums []int) *ListNode {
-	return &ListNode{
-		Val: nums[0],
-		Next: &ListNode {
-			Val: nums[1],
-			Next: &ListNode {
-				Val: nums[2],
-				Next: nil,
-			},
-		},
+	head := &ListNode{}
+	l := head
+	for i := 0; i < len(nums); i++ {
+		l.Next = &ListNode{Val: nums[i], Next: nil}
+		l = l.Next
 	}
+	return head.Next
 }
 
-func compare(n []int, n2 []int) bool {
-	return n[0] == n2[0] && n[1] == n2[1] && n[2] == n2[2]
+func compare(n1 []int, n2 []int) bool {
+	if len(n1) != len(n2) {
+		return false
+	}
+	r := true
+	for i := 0; i < len(n1); i++ {
+		r = r && (n1[i] == n2[i])
+	}
+	return r
 }
 
 func TestAddTwoNumbers(t *testing.T) {
 	l1 := [][]int{
 		{2, 4, 3},
+		{},
+		{1, 2, 3, 4, 5},
 	}
 	l2 := [][]int{
 		{5, 6, 4},
+		{},
+		{2, 2, 3, 4, 5},
 	}
 	target := [][]int{
 		{7, 0, 8},
+		{},
+		{3, 4, 6, 8, 0, 1},
 	}
-	
+
 	fmt.Printf("------------------------Leetcode Problem 2------------------------\n")
-	for i:=0; i < len(l1); i++ {
-		if compare(target[0], L2S(addTwoNumbers(S2L(l1[i]), S2L(l2[i])))) {
-			t.Fatalf("case %d fails\n", i)
+	for i := 0; i < len(l1); i++ {
+		res := L2S(addTwoNumbers(S2L(l1[i]), S2L(l2[i])))
+		if !compare(target[i], res) {
+			t.Fatalf("case %d fails, %v\n", i, res)
 		}
 	}
 }
